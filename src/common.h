bd073705ba6c0453d17b0e5f53dc4e32dcf6ed40
MINGtoMING <3575188313@qq.com>
2025-05-31 18:39:17 +0800

    add `find_char`

--- src/common.h
+++ src/common.h
@@ -1,36 +1,76 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include "moonbit.h"
+#include <string.h>
+
 #if defined(__x86_64__) || defined(_M_X64)
-    #if (defined(__AVX512F__) && defined(__AVX512BW__)) || defined(__AVX2__)
-        #include <immintrin.h>
-    #elif defined(__SSE2__)
-        #include <emmintrin.h>
-    #endif
+#if (defined(__AVX512F__) && defined(__AVX512BW__))
+#include <immintrin.h>
+#define USE_AVX512
+#elif defined(__AVX2__)
+#include <immintrin.h>
+#define USE_AVX2
+#elif defined(__SSE2__)
+#include <emmintrin.h>
+#define USE_SSE2
+#endif
 #elif defined(__aarch64__) || defined(_M_ARM64)
-    #if defined(__ARM_NEON)
-        #include <arm_neon.h>
-    #endif
+#if defined(__ARM_NEON)
+#include <arm_neon.h>
+#define USE_NEON
+#endif
 #endif
 
 #if defined(__GNUC__) || defined(__clang__)
-    #define likely(cond) __builtin_expect((cond), 1)
-    #define unlikely(cond) __builtin_expect((cond), 0)
-    #define always_inline __attribute__((always_inline)) inline
-    #define export __attribute__((visibility("default")))
+#define likely(cond) __builtin_expect((cond), 1)
+#define unlikely(cond) __builtin_expect((cond), 0)
+#define always_inline __attribute__((always_inline)) inline
+#define export __attribute__((visibility("default")))
+#define ctz32 __builtin_ctz
+#define ctz64 __builtin_ctzll
 #elif defined(_MSC_VER)
-    #define likely(cond) (cond)
-    #define unlikely(cond) (cond)
-    #define always_inline __forceinline inline
-    #define export __declspec(dllexport)
-#else
-    #define likely(cond) (cond)
-    #define unlikely(cond) (cond)
-    #define always_inline inline
-    #define export
-#endif
+#define likely(cond) (cond)
+#define unlikely(cond) (cond)
+#define always_inline __forceinline inline
+#define export __declspec(dllexport)
+#include <intrin.h>
+always_inline int32_t ctz32(const uint32_t bitmask)
+{
+    uint64_t count;
+    return _BitScanForward(&count, (uint64_t)bitmask);
+}
+always_inline int32_t ctz64(const uint64_t bitmask)
+{
+    uint64_t count;
+    return _BitScanForward(&count, bitmask);
+}
 
-#include "moonbit.h"
-#include <string.h>
+#else
+#define likely(cond) (cond)
+#define unlikely(cond) (cond)
+#define always_inline inline
+#define export
+always_inline int32_t ctz32(uint32_t bitmask)
+{
+    int32_t count = 0;
+    while ((bitmask & 1) == 0)
+    {
+        bitmask >>= 1;
+        ++count;
+    }
+    return count;
+}
+always_inline int32_t ctz64(uint64_t bitmask)
+{
+    int32_t count = 0;
+    while ((bitmask & 1) == 0)
+    {
+        bitmask >>= 1;
+        ++count;
+    }
+    return count;
+}
+#endif
 
 #endif // COMMON_H
--- src/find_char.c
+++ src/find_char.c
@@ -1,15 +1,16 @@
 #include "common.h"
 
-#if (defined(__x86_64__) || defined(_M_X64)) && (defined(__AVX512F__) && defined(__AVX512BW__))
+#if defined(USE_AVX512)
 
-export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_t needle)
+export int64_t find_char(const struct moonbit_view_t haystack, const uint32_t needle)
 {
     const int32_t len = haystack.len;
-    if (unlikely(len == 0))
-        return 0;
+    if (unlikely(!len))
+        return 0x100000000;
 
-    const uint16_t *ptr = (uint16_t *)haystack.buf + haystack.start;
-    const uint16_t *end = ptr + len;
+    const uint16_t *const start = (uint16_t *)haystack.buf + haystack.start;
+    const uint16_t *const end = start + len;
+    const uint16_t *ptr = start;
 
     if (likely(needle <= 0xFFFF))
     {
@@ -19,8 +20,9 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
         while (likely(ptr <= end - 32))
         {
             const __m512i src_blk = _mm512_loadu_si512((const __m512i *)ptr);
-            if (unlikely(_mm512_cmpeq_epi16_mask(src_blk, tgt_blk)))
-                return 1;
+            const uint32_t res_mask = _mm512_cmpeq_epi16_mask(src_blk, tgt_blk);
+            if (unlikely(res_mask))
+                return (int32_t)(ptr - start) + ctz32(res_mask);
             ptr += 32;
         }
 
@@ -30,14 +32,15 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             memset(buf, !tgt, sizeof(uint16_t) * 32);
             memcpy(buf, ptr, sizeof(uint16_t) * (end - ptr));
             const __m512i src_blk = _mm512_loadu_si512((const __m512i *)buf);
-            if (unlikely(_mm512_cmpeq_epi16_mask(src_blk, tgt_blk)))
-                return 1;
+            const uint32_t res_mask = _mm512_cmpeq_epi16_mask(src_blk, tgt_blk);
+            if (unlikely(res_mask))
+                return (int32_t)(ptr - start) + ctz32(res_mask);
         }
     }
     else
     {
         if (unlikely(len < 2))
-            return 0;
+            return 0x100000000;
 
         const uint16_t high_tgt = 0xD800 + ((needle - 0x10000) >> 10);
         const uint16_t low_tgt = 0xDC00 + ((needle - 0x10000) & 0x3FF);
@@ -47,13 +50,14 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
         while (likely(ptr <= end - 33))
         {
             const __m512i high_src_blk = _mm512_loadu_si512((const __m512i *)ptr);
-            const __mmask32 high_res_mask = _mm512_cmpeq_epi16_mask(high_src_blk, high_tgt_blk);
+            const uint32_t high_res_mask = _mm512_cmpeq_epi16_mask(high_src_blk, high_tgt_blk);
             if (unlikely(high_res_mask))
             {
                 const __m512i low_src_blk = _mm512_loadu_si512((const __m512i *)(ptr + 1));
-                const __mmask32 low_res_mask = _mm512_cmpeq_epi16_mask(low_src_blk, low_tgt_blk);
-                if (unlikely(high_res_mask & low_res_mask))
-                    return 1;
+                const uint32_t low_res_mask = _mm512_cmpeq_epi16_mask(low_src_blk, low_tgt_blk);
+                const uint32_t res_mask = high_res_mask & low_res_mask;
+                if (unlikely(res_mask))
+                    return (int32_t)(ptr - start) + ctz32(res_mask);
             }
             ptr += 32;
         }
@@ -63,29 +67,31 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             uint16_t buf[33] = {0};
             memcpy(buf, ptr, sizeof(uint16_t) * (end - ptr));
             const __m512i high_src_blk = _mm512_loadu_si512((const __m512i *)buf);
-            const __mmask32 high_res_mask = _mm512_cmpeq_epi16_mask(high_src_blk, high_tgt_blk);
+            const uint32_t high_res_mask = _mm512_cmpeq_epi16_mask(high_src_blk, high_tgt_blk);
             if (unlikely(high_res_mask))
             {
                 const __m512i low_src_blk = _mm512_loadu_si512((const __m512i *)(buf + 1));
-                const __mmask32 low_res_mask = _mm512_cmpeq_epi16_mask(low_src_blk, low_tgt_blk);
-                if (unlikely(high_res_mask & low_res_mask))
-                    return 1;
+                const uint32_t low_res_mask = _mm512_cmpeq_epi16_mask(low_src_blk, low_tgt_blk);
+                const uint32_t res_mask = high_res_mask & low_res_mask;
+                if (unlikely(res_mask))
+                    return (int32_t)(ptr - start) + ctz32(res_mask);
             }
         }
     }
-    return 0;
+    return 0x100000000;
 }
 
-#elif (defined(__x86_64__) || defined(_M_X64)) && defined(__AVX2__)
+#elif defined(USE_AVX2)
 
-export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_t needle)
+export int64_t find_char(const struct moonbit_view_t haystack, const uint32_t needle)
 {
     const int32_t len = haystack.len;
-    if (unlikely(len == 0))
-        return 0;
+    if (unlikely(!len))
+        return 0x100000000;
 
-    const uint16_t *ptr = (uint16_t *)haystack.buf + haystack.start;
-    const uint16_t *end = ptr + len;
+    const uint16_t *const start = (uint16_t *)haystack.buf + haystack.start;
+    const uint16_t *const end = start + len;
+    const uint16_t *ptr = start;
 
     if (likely(needle <= 0xFFFF))
     {
@@ -95,8 +101,9 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
         while (likely(ptr <= end - 16))
         {
             const __m256i src_blk = _mm256_loadu_si256((const __m256i *)ptr);
-            if (_mm256_movemask_epi8(_mm256_cmpeq_epi16(src_blk, tgt_blk)))
-                return 1;
+            const uint32_t res_mask = _mm256_movemask_epi8(_mm256_cmpeq_epi16(src_blk, tgt_blk));
+            if (unlikely(res_mask))
+                return (int32_t)(ptr - start) + ctz32(res_mask) / 2;
             ptr += 16;
         }
 
@@ -106,14 +113,16 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             memset(buf, !tgt, sizeof(uint16_t) * 16);
             memcpy(buf, ptr, sizeof(uint16_t) * (end - ptr));
             const __m256i src_blk = _mm256_loadu_si256((const __m256i *)buf);
-            if (_mm256_movemask_epi8(_mm256_cmpeq_epi16(src_blk, tgt_blk)))
-                return 1;
+            const uint32_t res_mask = _mm256_movemask_epi8(_mm256_cmpeq_epi16(src_blk, tgt_blk));
+            if (unlikely(res_mask))
+                return (int32_t)(ptr - start) + ctz32(res_mask) / 2;
         }
     }
     else
     {
         if (unlikely(len < 2))
-            return 0;
+            return 0x100000000;
+
         const uint16_t high_tgt = 0xD800 + ((needle - 0x10000) >> 10);
         const uint16_t low_tgt = 0xDC00 + ((needle - 0x10000) & 0x3FF);
         const __m256i high_tgt_blk = _mm256_set1_epi16((int16_t)high_tgt);
@@ -127,8 +136,9 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             {
                 const __m256i low_src_blk = _mm256_loadu_si256((const __m256i *)(ptr + 1));
                 const uint32_t low_res_mask = _mm256_movemask_epi8(_mm256_cmpeq_epi16(low_src_blk, low_tgt_blk));
-                if (unlikely(high_res_mask & low_res_mask))
-                    return 1;
+                const uint32_t res_mask = high_res_mask & low_res_mask;
+                if (unlikely(res_mask))
+                    return (int32_t)(ptr - start) + ctz32(res_mask) / 2;
             }
             ptr += 16;
         }
@@ -143,24 +153,26 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             {
                 const __m256i low_src_blk = _mm256_loadu_si256((const __m256i *)(buf + 1));
                 const uint32_t low_res_mask = _mm256_movemask_epi8(_mm256_cmpeq_epi16(low_src_blk, low_tgt_blk));
-                if (unlikely(high_res_mask & low_res_mask))
-                    return 1;
+                const uint32_t res_mask = high_res_mask & low_res_mask;
+                if (unlikely(res_mask))
+                    return (int32_t)(ptr - start) + ctz32(res_mask) / 2;
             }
         }
     }
-    return 0;
+    return 0x100000000;
 }
 
-#elif (defined(__x86_64__) || defined(_M_X64)) && defined(__SSE2__)
+#elif defined(USE_SSE2)
 
-export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_t needle)
+export int64_t find_char(const struct moonbit_view_t haystack, const uint32_t needle)
 {
     const int32_t len = haystack.len;
-    if (unlikely(len == 0))
-        return 0;
+    if (unlikely(!len))
+        return 0x100000000;
 
-    const uint16_t *ptr = (uint16_t *)haystack.buf + haystack.start;
-    const uint16_t *end = ptr + len;
+    const uint16_t *const start = (uint16_t *)haystack.buf + haystack.start;
+    const uint16_t *const end = start + len;
+    const uint16_t *ptr = start;
 
     if (likely(needle <= 0xFFFF))
     {
@@ -170,8 +182,9 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
         while (likely(ptr <= end - 8))
         {
             const __m128i src_blk = _mm_loadu_si128((const __m128i *)ptr);
-            if (_mm_movemask_epi8(_mm_cmpeq_epi16(src_blk, tgt_blk)))
-                return 1;
+            const uint32_t res_mask = _mm_movemask_epi8(_mm_cmpeq_epi16(src_blk, tgt_blk));
+            if (unlikely(res_mask))
+                return (int32_t)(ptr - start) + ctz32(res_mask) / 2;
             ptr += 8;
         }
 
@@ -181,14 +194,15 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             memset(buf, !tgt, sizeof(uint16_t) * 8);
             memcpy(buf, ptr, sizeof(uint16_t) * (end - ptr));
             const __m128i src_blk = _mm_loadu_si128((const __m128i *)buf);
-            if (_mm_movemask_epi8(_mm_cmpeq_epi16(src_blk, tgt_blk)))
-                return 1;
+            const uint32_t res_mask = _mm_movemask_epi8(_mm_cmpeq_epi16(src_blk, tgt_blk));
+            if (unlikely(res_mask))
+                return (int32_t)(ptr - start) + ctz32(res_mask) / 2;
         }
     }
     else
     {
         if (unlikely(len < 2))
-            return 0;
+            return 0x100000000;
         const uint16_t high_tgt = 0xD800 + ((needle - 0x10000) >> 10);
         const uint16_t low_tgt = 0xDC00 + ((needle - 0x10000) & 0x3FF);
         const __m128i high_tgt_blk = _mm_set1_epi16((int16_t)high_tgt);
@@ -202,8 +216,9 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             {
                 const __m128i low_src_blk = _mm_loadu_si128((const __m128i *)(ptr + 1));
                 const uint32_t low_res_mask = _mm_movemask_epi8(_mm_cmpeq_epi16(low_src_blk, low_tgt_blk));
-                if (unlikely(high_res_mask & low_res_mask))
-                    return 1;
+                const uint32_t res_mask = high_res_mask & low_res_mask;
+                if (unlikely(res_mask))
+                    return (int32_t)(ptr - start) + ctz32(res_mask) / 2;
             }
             ptr += 8;
         }
@@ -218,24 +233,31 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             {
                 const __m128i low_src_blk = _mm_loadu_si128((const __m128i *)(buf + 1));
                 const uint32_t low_res_mask = _mm_movemask_epi8(_mm_cmpeq_epi16(low_src_blk, low_tgt_blk));
-                if (unlikely(high_res_mask & low_res_mask))
-                    return 1;
+                const uint32_t res_mask = high_res_mask & low_res_mask;
+                if (unlikely(res_mask))
+                    return (int32_t)(ptr - start) + ctz32(res_mask) / 2;
             }
         }
     }
-    return 0;
+    return 0x100000000;
 }
 
-#elif (defined(__aarch64__) || defined(_M_ARM64)) && defined(__ARM_NEON)
+#elif defined(USE_NEON)
 
-export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_t needle)
+always_inline uint64_t to_bitmask(const uint16x8_t words)
+{
+    return (uint64_t)vshrn_n_u16(words, 8) & 0x8080808080808080ull;
+}
+
+export int64_t find_char(const struct moonbit_view_t haystack, const uint32_t needle)
 {
     const int32_t len = haystack.len;
-    if (unlikely(len == 0))
-        return 0;
+    if (unlikely(!len))
+        return 0x100000000;
 
-    const uint16_t *ptr = (uint16_t *)haystack.buf + haystack.start;
-    const uint16_t *end = ptr + len;
+    const uint16_t *const start = (uint16_t *)haystack.buf + haystack.start;
+    const uint16_t *const end = start + len;
+    const uint16_t *ptr = start;
 
     if (likely(needle <= 0xFFFF))
     {
@@ -245,8 +267,9 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
         while (likely(ptr <= end - 8))
         {
             const uint16x8_t src_blk = vld1q_u16(ptr);
-            if (unlikely(vmaxvq_u16(vceqq_u16(src_blk, tgt_blk))))
-                return 1;
+            const uint16x8_t res_blk = vceqq_u16(src_blk, tgt_blk);
+            if (unlikely(vmaxvq_u16(res_blk)))
+                return (int32_t)(ptr - start) + ctz64(to_bitmask(res_blk)) / 8;
             ptr += 8;
         }
 
@@ -256,14 +279,15 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
             memset(buf, !tgt, sizeof(uint16_t) * 8);
             memcpy(buf, ptr, sizeof(uint16_t) * (end - ptr));
             const uint16x8_t src_blk = vld1q_u16(buf);
-            if (unlikely(vmaxvq_u16(vceqq_u16(src_blk, tgt_blk))))
-                return 1;
+            const uint16x8_t res_blk = vceqq_u16(src_blk, tgt_blk);
+            if (unlikely(vmaxvq_u16(res_blk)))
+                return (int32_t)(ptr - start) + ctz64(to_bitmask(res_blk)) / 8;
         }
     }
     else
     {
         if (unlikely(len < 2))
-            return 0;
+            return 0x100000000;
 
         const uint16_t high_tgt = 0xD800 + ((needle - 0x10000) >> 10);
         const uint16_t low_tgt = 0xDC00 + ((needle - 0x10000) & 0x3FF);
@@ -279,8 +303,11 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
                 const uint16x8_t low_src_blk = vld1q_u16(ptr + 1);
                 const uint16x8_t low_res_blk = vceqq_u16(low_src_blk, low_tgt_blk);
                 if (unlikely(vmaxvq_u16(low_res_blk)))
-                    if (unlikely(vmaxvq_u16(vandq_u16(high_res_blk, low_res_blk))))
-                        return 1;
+                {
+                    const uint16x8_t res_blk = vandq_u16(high_res_blk, low_res_blk);
+                    if (unlikely(vmaxvq_u16(res_blk)))
+                        return (int32_t)(ptr - start) + ctz64(to_bitmask(res_blk)) / 8;
+                }
             }
             ptr += 8;
         }
@@ -296,85 +323,91 @@ export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_
                 const uint16x8_t low_src_blk = vld1q_u16(buf + 1);
                 const uint16x8_t low_res_blk = vceqq_u16(low_src_blk, low_tgt_blk);
                 if (unlikely(vmaxvq_u16(low_res_blk)))
-                    if (unlikely(vmaxvq_u16(vandq_u16(high_res_blk, low_res_blk))))
-                        return 1;
+                {
+                    const uint16x8_t res_blk = vandq_u16(high_res_blk, low_res_blk);
+                    if (unlikely(vmaxvq_u16(res_blk)))
+                        return (int32_t)(ptr - start) + ctz64(to_bitmask(res_blk)) / 8;
+                }
             }
         }
     }
-    return 0;
+    return 0x100000000;
 }
 
 #else
 
-always_inline int32_t has_zero_word(const uint64_t quadword)
+always_inline uint64_t to_bitmask(const uint64_t words)
 {
-    return ((quadword - 0x0001000100010001ULL) & ~(quadword) & 0x8000800080008000ULL) != 0;
+    return (~words - 0x0001000100010001ULL) & words & 0x8000800080008000ULL;
 }
 
-always_inline uint64_t splat_word(const uint16_t word)
+always_inline uint64_t splat(const uint16_t word)
 {
     return (uint64_t)word * 0x0001000100010001ULL;
 }
 
-export int32_t contains_char(const struct moonbit_view_t haystack, const uint32_t needle)
+export int64_t find_char(const struct moonbit_view_t haystack, const uint32_t needle)
 {
     const int32_t len = haystack.len;
-    if (unlikely(len == 0))
-        return 0;
+    if (unlikely(!len))
+        return 0x100000000;
 
-    const uint16_t *ptr = (uint16_t *)haystack.buf + haystack.start;
-    const uint16_t *end = ptr + len;
+    const uint16_t *const start = (uint16_t *)haystack.buf + haystack.start;
+    const uint16_t *const end = start + len;
+    const uint16_t *ptr = start;
 
     if (likely(needle <= 0xFFFF))
     {
         const uint16_t tgt = (uint16_t)needle;
-        const uint64_t tgt_blk = splat_word(tgt);
+        const uint64_t tgt_blk = splat(tgt);
 
         while (likely(ptr <= end - 4))
         {
             uint64_t src_blk;
             memcpy(&src_blk, ptr, sizeof(uint64_t));
-            if (unlikely(has_zero_word(src_blk ^ tgt_blk)))
-                return 1;
+            const uint64_t res_mask = to_bitmask(~(src_blk ^ tgt_blk));
+            if (unlikely(res_mask))
+                return (int32_t)(ptr - start) + ctz64(res_mask) / 16;
             ptr += 4;
         }
 
         while (unlikely(ptr < end))
         {
-            if (unlikely(ptr[0] == tgt))
-                return 1;
+            if (unlikely(*ptr == tgt))
+                return (int32_t)(ptr - start);
             ++ptr;
         }
     }
     else
     {
         if (unlikely(len < 2))
-            return 0;
+            return 0x100000000;
 
         const uint16_t high_tgt = 0xD800 + ((needle - 0x10000) >> 10);
         const uint16_t low_tgt = 0xDC00 + ((needle - 0x10000) & 0x3FF);
-        const uint64_t high_tgt_blk = splat_word(high_tgt);
-        const uint64_t low_tgt_blk = splat_word(low_tgt);
+        const uint64_t high_tgt_blk = splat(high_tgt);
+        const uint64_t low_tgt_blk = splat(low_tgt);
 
         while (likely(ptr <= end - 5))
         {
             uint64_t high_src_blk, low_src_blk;
             memcpy(&high_src_blk, ptr, sizeof(uint64_t));
             memcpy(&low_src_blk, ptr + 1, sizeof(uint64_t));
-            if (unlikely(has_zero_word((high_src_blk ^ high_tgt_blk) | (low_src_blk ^ low_tgt_blk))))
-                return 1;
+            const uint64_t res_mask = to_bitmask(~((high_src_blk ^ high_tgt_blk) | (low_src_blk ^ low_tgt_blk)));
+            if (unlikely(res_mask))
+                return (int32_t)(ptr - start) + ctz64(res_mask) / 16;
             ptr += 4;
         }
 
         while (unlikely(ptr < end - 1))
         {
-            if (unlikely(ptr[0] == high_tgt && ptr[1] == low_tgt))
-                return 1;
+            if (unlikely(*ptr == high_tgt && *(ptr + 1) == low_tgt))
+                return (int32_t)(ptr - start);
             ++ptr;
         }
     }
 
-    return 0;
+    return 0x100000000;
 }
 
 #endif
